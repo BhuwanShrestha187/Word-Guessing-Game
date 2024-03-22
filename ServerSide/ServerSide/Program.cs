29d5cfd6d1e3a72bf3b7d19eb26728620431c701
Bhuwan Shrestha <bshrestha2146@conestogac.on.ca>
2024-03-22 14:37:31 -0400

    Initial Commit

---
+++ ServerSide/ServerSide/Program.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Configuration; 
+namespace ServerSide
+{
+    internal class Program
+    {
+        static string directoryPath = "D:\\Conestoga College";
+        static List<string> wordsToFind;
+        static TcpListener server;
+        static bool isClientClosed = false;
+        
+
+        static readonly object lockObject = new object();
+
+        // Use ManualResetEvent to signal the shutdown
+        static ManualResetEvent shutdownEvent = new ManualResetEvent(false);
+
+
+        static void Main(string[] args)
+        {
+            string ipAddress = ConfigurationManager.AppSettings["ipAddress"];
+            server = null; 
+            try
+            {
+                //Set the TcpListener on port 1111
+                Int32 port = 1111; 
+                IPAddress localAddr = IPAddress.Parse(ipAddress);
+                server = new TcpListener(localAddr, port);
+                server.Start();
+                while (!isClientClosed)
+                {
+                    
+                    try
+                    {
+                        TcpClient client = server.AcceptTcpClient();
+                        // Create a new thread for each client
+                        Thread clientThread = new Thread(() => HandleClient(client));
+                        clientThread.Start();
+                    }
+                    catch (SocketException)
+                    {
+                        // SocketException will be thrown when the server is stopped
+                        break;
+                    }
+                }
+            }
+            finally
+            {
+                // Set the shutdown event and wait for all clients to finish
+                shutdownEvent.Set();
+                
+            }
+            server.Stop();
+        }
+
+        static void HandleClient(TcpClient client)
+        {
+            StreamReader reader = new StreamReader(client.GetStream());
+            StreamWriter writer = new StreamWriter(client.GetStream());
+
+            try
+            {
+                if (client != null)
+                {
+                    List<string> guessedWord = new List<string>(); 
+
+                    while (true)
+                    {   
+                        string textFilePath = ChooseTextFile(directoryPath);
+                        string[] lines = File.ReadAllLines(textFilePath);
+                        string characterString;
+                        int numberOfWords;
+
+                        // Ensure that the file has at least two lines
+                        if (lines.Length >= 2)
+                        {
+                            // Extract information from the file
+                             characterString = lines[0].Substring(0, Math.Min(lines[0].Length, 80));
+                             numberOfWords = CountWords(lines[0]); 
+                            // Send information to the client
+                            writer.WriteLine(characterString);
+                            writer.WriteLine(numberOfWords);
+                            
+                            writer.Flush(); // Ensure that the data is sent immediately
+
+                            // You may add additional logic here to send the list of words if needed
+                           wordsToFind = lines[0].Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                            int correctWords = 0;
+                            // Notify the user and get their guesses
+                            while (numberOfWords > 0)
+                            {
+                                string userGuess = reader.ReadLine();
+                               
+                                if (userGuess.StartsWith("endgame"))
+                                {
+                                    writer.WriteLine("Ended Communication with the server!!. You guessed " + correctWords.ToString() + " words correct!!");
+                                    writer.Flush(); 
+                                    
+                                    return;        
+                                }
+
+                                // Check if the user guess is in the list of words
+                                      if(wordsToFind.Contains(userGuess))
+                                        {
+                                            guessedWord.Add(userGuess);
+                                            numberOfWords--;
+                                            if (numberOfWords > 0)
+                                            {
+                                                writer.WriteLine($"Correct! {numberOfWords} word(s) still to find.");
+                                                correctWords++;
+                                            }
+
+                                            else
+                                            {
+                                                writer.WriteLine("Congratulations! You've found all the words!");
+                                                writer.Flush();
+
+                                                break;
+                                            }
+                                        }
+                                        else
+                                        {
+
+                                    if (guessedWord.Contains(userGuess))
+                                    {
+                                        writer.WriteLine("Note: You guessed this number already!!");
+                                        writer.Flush();
+                                        continue;
+                                    }
+
+                                    writer.WriteLine("Incorrect! Try Again!!");
+                                        }
+                                    
+                                
+
+                                    writer.Flush();  
+                            }                               
+                        }
+                        else
+                        {
+                            writer.WriteLine("Invalid format of the text file.");
+                        }
+
+                        string playAgainResponse = reader.ReadLine();
+                        if (playAgainResponse.Equals("yes", StringComparison.OrdinalIgnoreCase))
+                        {
+                            continue;
+
+                        }
+                        else
+                        {
+
+                            client.Close();
+                            break;
+                            
+                        }
+
+                    }  
+                   
+                }
+                else
+                {
+                    writer.WriteLine("Client is null");
+                }
+            }
+            catch (Exception ex)
+            {
+                writer.WriteLine($"Error handling client: {ex.Message}");
+            }
+            finally
+            {
+                writer.Close();
+                reader.Close();
+                client.Close(); 
+            }
+        }
+
+        static int CountWords(string text)
+        {
+            int wordCount = text.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)
+                           .Count();
+
+            return wordCount;
+        }
+        static string ChooseTextFile(string directory)
+        {
+            string[] files = Directory.GetFiles(directory, "*.txt");
+            if (files.Length == 0)
+            {
+                throw new Exception("No text files found in the specified directory!!");
+            }
+
+            string chosenFile = files[new Random().Next(files.Length)];
+            return chosenFile;
+        }
+
+      
+
+       
+    }
+}
---
+++ UserSide/UserSide/MainWindow.xaml.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace UserSide
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string ipAddress;
+        private int port;
+        private string username;
+        private int timeLimit;
+
+
+        private TcpClient client;
+        private NetworkStream stream;
+        private StreamReader reader;
+        private StreamWriter writer;
+        private int totalWords;
+
+        private int numCorrectGuess;
+        private int numIncorrectGuess;
+        private bool isServerShuttingDown = false;
+
+        private DispatcherTimer timer;
+        private int remainingTime;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Read values from textboxes
+                ipAddress = txtIpAddress.Text;
+                port = int.Parse(txtPort.Text);
+
+                // Connect to the server asynchronously
+                client = new TcpClient();
+                await client.ConnectAsync(ipAddress, port);
+
+                connectionGrid.Visibility = Visibility.Collapsed;
+                gameGrid.Visibility = Visibility.Visible;
+                stream = client.GetStream();
+                reader = new StreamReader(stream);
+                writer = new StreamWriter(stream);
+                // Read the 80-character string from the server
+                byte[] buffer = new byte[80];
+                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                string serverString = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+                // Read the number of words from the server
+                bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                totalWords = int.Parse(Encoding.ASCII.GetString(buffer, 0, bytesRead));
+                //Before updating to UI remove whnitespaces between the words to confuse the clients
+                serverString = serverString.Replace(" ", string.Empty);
+                // Update UI with the received information
+                secretWordLabel.Text = "Words to Guess: " + serverString;
+                totalWordsLabel.Text = "Total Numbers: " + totalWords;
+
+
+                // Initialize and start the timer
+                timeLimit = int.Parse(txtTimeLimit.Text);
+                remainingTime = timeLimit;
+                timer = new DispatcherTimer();
+                timer.Interval = TimeSpan.FromSeconds(1);
+                timer.Tick += Timer_Tick;
+                timer.Start();
+            }
+
+            catch (Exception ex)
+            {
+                // Handle connection error
+
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            remainingTime--;
+
+            if (remainingTime <= 0)
+            {
+                timer.Stop();
+                MessageBox.Show("Oooo....Time's up!", "Game Completed", MessageBoxButton.OK);
+                writer.WriteLine("endgame");
+                Application.Current.Shutdown();
+            }
+        }
+
+
+
+
+        private async void SubmitGuess_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string userGuess = txtGuess.Text;
+
+                //Send the user;s guess to the server
+                await writer.WriteLineAsync(userGuess);
+                await writer.FlushAsync();
+
+                //Receive and process the user guess
+                string response = await reader.ReadLineAsync();
+
+                //Update the UI 
+                UpdateUI(response);
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private async void UpdateUI(string response)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                if (response != null && response.StartsWith("Server is shutting down."))
+                {
+                    // Handle server shutdown gracefully
+                    isServerShuttingDown = true;
+                    MessageBox.Show("The server is shutting down. The game will now exit.");
+                    Application.Current.Shutdown();
+                }
+                if (response != null && response.StartsWith("Correct!"))
+                {
+                    numCorrectGuess++;
+                    correctStatus.Text = response;
+                    alreadyGuessedStatus.Text = "";
+                }
+
+                if (response != null && response.StartsWith("Note:"))
+                {
+                    alreadyGuessedStatus.Text = response;
+                }
+
+                if (response != null && response.StartsWith("Incorrect!"))
+                {
+                    numIncorrectGuess++;
+                    incorrectStatus.Text = response + " Incorrect Count: " + numIncorrectGuess.ToString();
+                    alreadyGuessedStatus.Text = "";
+                }
+
+                if (response != null && response.StartsWith("Congratulations!"))
+                {
+                   
+                    MessageBoxResult result = MessageBox.Show("Congratulations! You've found all the words!\nDo you want to play again?", "Game Completed", MessageBoxButton.YesNo);
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        StartNewGame();
+                    }
+
+                    else
+                    {
+                        SendEndGameRequest();
+                    }
+                }
+
+            });
+        }
+
+
+        private async void StartNewGame()
+        {
+            correctStatus.Text = "";
+            incorrectStatus.Text = "";
+            alreadyGuessedStatus.Text = "";
+
+            try
+            {
+                writer.WriteLine("yes");
+                writer.Flush();
+
+                // Check if the client is still connected
+                if (client.Connected)
+                {
+                    byte[] buffer = new byte[80];
+                    int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                    // Check if the client is still connected after reading from the stream
+                    if (client.Connected)
+                    {
+                        string serverString = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+                        // Read the number of words from the server
+                        bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                        totalWords = int.Parse(Encoding.ASCII.GetString(buffer, 0, bytesRead));
+
+                        // Before updating to UI remove whitespaces between the words to confuse the clients
+                        serverString = serverString.Replace(" ", string.Empty);
+
+                        // Update UI with the received information
+                        secretWordLabel.Text = "Words to Guess: " + serverString;
+                        totalWordsLabel.Text = "Total Numbers: " + totalWords;
+                    }
+                    else
+                    {
+                        // Handle the case where the client is no longer connected (e.g., the application is closed)
+                        MessageBox.Show("Connection closed. Game cannot be started.");
+                    }
+
+                    // Restart the timer for the new game
+                    remainingTime = timeLimit;
+                    timer.Start();
+                }
+                else
+                {
+                    // Handle the case where the client is no longer connected (e.g., the application is closed)
+                    MessageBox.Show("Connection closed. Game cannot be started.");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any other exceptions
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void EndGame_Click(object sender, RoutedEventArgs e)
+        {
+
+            SendEndGameRequest();
+        }
+
+        private async void SendEndGameRequest()
+        {
+            try
+            {
+                MessageBoxResult result = MessageBox.Show("Are you sure you want to end the game?", "Confirm End Game", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.Yes)
+                {
+                    // User confirmed, send a request to the server
+                    await writer.WriteLineAsync("endgame");
+                    await writer.FlushAsync();
+                    Application.Current.Shutdown();
+                }
+                else
+                {
+                    return;
+                }
+                
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void Populate_Click(object sender, RoutedEventArgs e)
+        {
+            txtIpAddress.Text = "52.151.251.103";
+            txtPort.Text = "1111";
+            txtUserName.Text = "Bhuwan";
+            txtTimeLimit.Text = "50";
+        }
+    }
+}
